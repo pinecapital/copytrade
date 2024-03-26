10c31e36bfd8d04a6ea3bd942de989d5240137d1
pinecapital <54320808+pinecapital@users.noreply.github.com>
2024-03-26 16:59:51 +0530

    TEST ADDED

--- testScriptCodeConverter.py
+++ testScriptCodeConverter.py
@@ -4,6 +4,7 @@ from ScripCodeConverter import ScripConverter
 csv_url = "https://openapi.5paisa.com/VendorsAPI/Service1.svc/ScripMaster/segment/All"
 converter = ScripConverter(csv_url)
 
-fyers_symbol = "NSE:BANKNIFTY24MAR46700CE"
+fyers_symbol = "NSE:IDEA-EQ"
 scrip_code = converter.convert_symbol(fyers_symbol)
 
+print(f"5paisa scrip code: {scrip_code}")
